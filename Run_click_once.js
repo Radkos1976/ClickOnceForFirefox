cb800906cea336c1bada0c247db10c9970fee3c7
Radomir Kosobucki <radomir.kosobucki@sits.pl>
2020-11-10 09:44:35 +0100

    add

--- Run_click_once.js
+++ Run_click_once.js
@@ -1,31 +1,56 @@
-var ar=';http://sitsifsapp1.sits.local:59080';
-function listener(details) {
-  if (ar.indexOf(details.url.substring(1,35))!=-1) {
-   return  {redirectUrl:'ifs:' + details.url };
- } else {
-  return {}};
+let url_list =  [];
+
+function get_Allowed_Domains () {
+  browser.storage.local.get("allowed_domains").then((results) => {
+		url_list = results.allowed_domains;
+		if (url_list == undefined) {
+      url_list =  ['http://sitsifsapp1.sits.local:59080','http://10.1.1.29:3000','http://10.0.1.29:3000'];
+      browser.storage.local.set({allowed_domains: url_list});
+		}
+	}).catch((err) => {console.log('Error retrieving storage: '+err.message);});
 }
-function  closetabs(detalis) {
-  if (ar.indexOf(detalis.url.substring(1,35))!=-1 & detalis.url.substring(1,3)!='ifs') {
+get_Allowed_Domains();
+function Is_Allowed(Present_url){
+    let allow=false;
+    for (var j=0; j<url_list.length; j++)
+    {
+      if (Present_url.indexOf(url_list[j])==0)
+      {
+        allow=true;
+        break;
+      }
+    }
+    return allow
+};
+function listener(req) {
+  if (Is_Allowed(req.url)) {
+    return  {redirectUrl:'ifs:' + req.url }; // redirect to registered URI
+  } else {
+    return {}};
+};
+function closetabs(req) {
+  if (Is_Allowed(req.url) & req.url.substring(1,3)!='ifs') {
     var hiding = browser.tabs.remove(
-        detalis.tabId         // integer or integer array
+        req.tabId
       );
   }
-}
+};
+
+
 browser.webRequest.onHeadersReceived.addListener(
   closetabs,
   {
-    urls: ["<all_urls>"],
-    types: ["main_frame"]
+    urls: ["*://*/*.application*"],
+    types: ["main_frame", "sub_frame", "other"]
   },
 [ "responseHeaders"]
 
-)
+);
 browser.webRequest.onBeforeSendHeaders.addListener(
 		listener,
 		{
-			urls: ["<all_urls>"],
-			types: ["main_frame"]
+			urls: ["*://*/*.application*"],
+			types: ["main_frame", "sub_frame", "other"]
 		},
 		["blocking"]
 	);
